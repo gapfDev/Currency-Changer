077fd5ef9028f2dc5063aa01893e62333cf9ff7d
Gabriel P <gperezflamenco@extraspace.com>
2020-11-26 11:32:47 -0700

    *** Clean Application class.

--- app/src/main/java/com/alxdev/two/moneychanger/AppApplication.kt
+++ app/src/main/java/com/alxdev/two/moneychanger/AppApplication.kt
@@ -5,35 +5,4 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class AppApplication : Application() {
-
-//    init {
-//        instance = this
-//    }
-
-//    companion object {
-//        private var instance: AppApplication? = null
-//        private fun applicationCon(): Context {
-//            return instance!!.applicationContext
-//        }
-
-//       val currencyListRetrofitBuild
-//            get() = MockRetrofitController.retrofitBuild(
-//                Constants.API.BASE_URL
-//            )
-
-//        val currencyCountryRetrofitBuild
-//            get() = MockRetrofitController.retrofitBuild(
-//                Constants.API.COUNTRY_BASE_URL
-//            )
-////
-//        val mockRetrofit: MockRetrofitController
-//            get() = MockRetrofitController
-//
-//
-//        private val moneyChangerDataBase
-//            get() = MoneyChangerDataBase.getInstance(applicationCon())
-//
-//        val changerRepository
-//            get() = ChangerRepository.getInstance(moneyChangerDataBase)
-//    }
 }
